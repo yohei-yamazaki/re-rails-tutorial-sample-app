20e763d7b7549b7a1f7c468ee5c74846d4470d36
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-02 16:20:44 +0900

    rubocop autocorrect spec/requests/static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,35 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages/home" do
-    it "should get home" do
+RSpec.describe 'StaticPages', type: :request do
+
+  describe 'GET /static_pages/home' do
+    before do
       get static_pages_home_path
+    end
+
+    it 'should get home' do
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /static_pages/help" do
-    it "should get help" do
+  describe 'GET /static_pages/help' do
+    before do
       get static_pages_help_path
+    end
+
+    it 'should get help' do
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /static_pages/about" do
-    it "should get about" do
+  describe 'GET /static_pages/about' do
+    before do
       get static_pages_about_path
+    end
+
+    it 'should get about' do
       expect(response).to have_http_status(200)
     end
   end
