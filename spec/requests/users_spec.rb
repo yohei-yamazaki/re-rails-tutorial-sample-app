fe3ef6276c735935c53640ec952926bd974a5872
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-06 14:54:52 +0900

    fixup users/new request spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /users/new" do
     it "should get new" do
-      get users_new_url
+      get signup_path
       expect(response).to have_http_status(200)
     end
   end
