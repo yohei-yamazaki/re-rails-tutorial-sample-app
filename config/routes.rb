210fbce1f4f2494b19bac0316a258e631a2aff9a
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-06 10:24:05 +0900

    [red] fixup routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
