b4b61d4039857d1831ee59fa59fdc7ba8d24cbbb
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-09 17:58:21 +0900

    [red] Add GET /users/:id path test case

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let!(:user) { FactoryBot.create(:user) }
+  describe 'GET /users/:id' do
+    it 'should get show' do
+      get user_path(user)
+      expect(response).to have_http_status(200)
+    end
+  end
+
   describe 'GET /users/new' do
     it 'should get new' do
       get signup_path
