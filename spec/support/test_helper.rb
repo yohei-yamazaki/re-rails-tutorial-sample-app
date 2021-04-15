a0e5110d9be97cb14a80ade3af5c33b3e00f4512
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-15 18:08:11 +0900

    fixup login as test helper to accessed http method and path

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -5,7 +5,11 @@ module TestHelper
   end
 
   # テストユーザーとしてログインする
-  def log_in_as(user)
-    session[:user_id] = user.id
+  def log_in_as(user, remember_me: '1')
+    post login_path, params: { session: {
+      email: user.email,
+      password: user.password,
+      remember_me: remember_me
+    } }
   end
 end
