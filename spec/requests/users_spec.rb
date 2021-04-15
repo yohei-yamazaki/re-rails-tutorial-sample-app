8bbddb3c35ba4c1f5c910f7a9bf406031efc2ed2
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-15 17:10:12 +0900

    fixup edit and update to allow logged in user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'Get /users/:id/edit' do
+    context 'not logged in' do
+      it 'redirect edit' do
+        get edit_user_path(user)
+        expect(flash).not_to be_empty
+        expect(response).to redirect_to login_url
+      end
+    end
+  end
+
+  describe 'Patch /users/:id' do
+    context 'not logged in' do
+      it 'redirect update' do
+        patch user_path(user), params: { user: { name: user.name, email: user.email } }
+        expect(flash).not_to be_empty
+        expect(response).to redirect_to login_url
+      end
+    end
+  end
 end
