caa56c91423b74aef0a21b4d84258d5e9cf7a862
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-15 10:19:54 +0900

    Add test helper

---
+++ spec/support/test_helper.rb
@@ -0,0 +1,11 @@
+module TestHelper
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
