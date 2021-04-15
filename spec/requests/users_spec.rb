1335102a0769e294b5a15830e5f723e4d53c922e
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-15 18:08:56 +0900

    Update correct_user  before action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   let!(:user) { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user, { name: 'other user' }) }
+
   describe 'GET /users/:id' do
     it 'should get show' do
       get user_path(user)
@@ -16,23 +18,20 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  describe 'Get /users/:id/edit' do
-    context 'not logged in' do
-      it 'redirect edit' do
-        get edit_user_path(user)
-        expect(flash).not_to be_empty
-        expect(response).to redirect_to login_url
-      end
+  describe 'before_action: :correct_user' do
+    before do
+      log_in_as(other_user)
     end
-  end
 
-  describe 'Patch /users/:id' do
-    context 'not logged in' do
-      it 'redirect update' do
-        patch user_path(user), params: { user: { name: user.name, email: user.email } }
-        expect(flash).not_to be_empty
-        expect(response).to redirect_to login_url
-      end
+    it 'redirects edit when logged in as wrong user' do
+      get edit_user_path(user, { 'user_id': 5 })
+      expect(flash).not_to be_empty
+      expect(response).to redirect_to login_url
+    end
+    it 'redirects update when logged in as wrong user' do
+      patch user_path(user), params: { user: { name: user.name, email: user.email } }
+      expect(flash).not_to be_empty
+      expect(response).to redirect_to login_url
     end
   end
 end
