1fa94909ef75a36cfebcf99a28fa85db5160ab46
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-15 10:22:26 +0900

    Add current user persistent session test in session helper

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SessionsHelper. For example:
-#
-# describe SessionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe SessionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user, name: 'Example User') }
+
+  before do
+    remember(user)
+  end
+
+  describe 'current_user' do
+    it 'returns right user when session is nil' do
+      expect(user).to eq current_user
+      expect(is_logged_in?).to be_truthy
+    end
+
+    it 'returns nil when remember digest is wrong' do
+      user.update_attribute(:remember_digest, User.digest(User.new_token))
+      expect(current_user).to be nil
+    end
+  end
 end
