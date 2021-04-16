91ef636d48b37d5fbc9a24d2052bb0beb1fdf870
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-16 15:34:53 +0900

    fixup user_params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_digest)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def logged_in_user
