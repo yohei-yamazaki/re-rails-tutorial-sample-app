fe62945bec5138232a88801e37eb84d3dc170b62
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-19 15:20:18 +0900

    Add account_activation test case in user mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,32 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  let(:user) { FactoryBot.create(:user) }
+  describe 'account_activation' do
+    let(:mail) { UserMailer.account_activation(user) }
+    before do
+      user.activation_token = User.new_token
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Account activation')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['noreply@example.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match(user.name)
+      expect(mail.body.encoded).to match(user.activation_token)
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
     end
   end
 
-  describe "password_reset" do
+  xdescribe 'password_reset' do
     let(:mail) { UserMailer.password_reset }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Password reset')
+      expect(mail.to).to eq(['to@example.org'])
+      expect(mail.from).to eq(['from@example.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hi')
     end
   end
-
 end
